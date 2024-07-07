d44a0cc232e0563fc0da5d6996bb90da5dcf7da2
Leonardo Malcotti <leo@Leonardos-Air.lan>
2024-07-07 14:31:29 +0200

    initialized spring app

---
+++ src/main/java/com/sittingspot/tagextractor/TagextractorApplication.java
@@ -0,0 +1,13 @@
+package com.sittingspot.tagextractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagextractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagextractorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sittingspot/tagextractor/TagextractorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sittingspot.tagextractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagextractorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
